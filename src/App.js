234ba4ebe8858ad3d6a3be36e9f2285198095def
jani.vaismaa <jani.vaismaa@PF3KN91A>
2022-12-17 10:00:56 +0200

    1.1 tehty

--- src/App.js
+++ src/App.js
@@ -1,16 +1,44 @@
-const Header = () => {     
-<div>
-<Header course={course} />
-</div>
+const Header = (course) => {
+  return(
+  <h1>{course.name}</h1>
+  )
 }
 
+const Content = (lista) => {
+  return(
+  <>
+    <p>
+    {lista.lista[0].name} {lista.lista[0].exercises}
+  </p>
+  <p>
+    {lista.lista[1].name} {lista.lista[1].exercises}
+  </p>
+  <p>
+    {lista.lista[2].name} {lista.lista[2].exercises}
+  </p>
+  </>
+
+  )
+}
+
+const Total = (lista) => {
+  return (<p>Number of exercises {lista.lista[0].exercises+lista.lista[1].exercises+lista.lista[2].exercises}</p>)
+}
 
 const App = () => {
   const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
-  }
+  const lista = [
+    { name: "Fundamentals of React", exercises: 10, },
+    { name: "Using props to pass data", exercises: 7,},
+    { name: "State of a component", exercises: 14, },]
+
+  return (
+    <div>
+      <Header name={course}/>
+      <Content lista={lista}/>
+      <Total lista={lista}/>
+    </div>
+  )
+}
+
+export default App
