6d7d75755490e3fda2a9eadf8a274a30149f6b61
jani.vaismaa <jani.vaismaa@PF3KN91A>
2022-12-17 10:53:37 +0200

    1.5

--- src/App.js
+++ src/App.js
@@ -28,17 +28,30 @@ const Total = (lista) => {
 }
 
 const App = () => {
-  const course = 'Half Stack application development'
-  const lista = [
-    { name: "Fundamentals of React", exercises: 10, },
-    { name: "Using props to pass data", exercises: 7,},
-    { name: "State of a component", exercises: 14, },]
+  
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
 
   return (
     <div>
-      <Header name={course}/>
-      <Content lista={lista}/>
-      <Total lista={lista}/>
+      <Header name={course.name}/>
+      <Content lista={course.parts}/>
+      <Total lista={course.parts}/>
     </div>
   )
 }
